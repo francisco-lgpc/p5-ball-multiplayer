19ee716461bce601d71382f772803f341aebb6cb
Francisco P Coutinho <francisco.lgpc@gmail.com>
2020-04-10 21:23:06 +0200

    setup simple ball rendering

--- src/sketch.js
+++ src/sketch.js
@@ -1,3 +1,10 @@
-function setup() {}
+function setup() {
+  createCanvas(1000, 500)
+  background(0)
 
-function draw() {}
+  const ball = new Ball(50, 500, 250)
+
+  console.log(ball.r)
+
+  ball.show()
+}
