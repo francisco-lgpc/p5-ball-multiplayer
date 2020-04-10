6d6bc148b241b78a449dfd16c0539276fabbac67
Francisco P Coutinho <francisco.lgpc@gmail.com>
2020-04-11 01:00:37 +0200

    implement socket in client

--- client/src/sketch.js
+++ client/src/sketch.js
@@ -1,10 +1,60 @@
+const socket = new WebSocket('ws://localhost:8999')
+
+socket.onmessage = event => {
+  const data = JSON.parse(event.data);
+  const state = data.state;
+
+  if (!state) return
+
+  background(0)
+
+  Object.entries(state.balls).forEach(([id, pos]) => {
+    new Ball(50, pos.x, pos.y).show();
+  })
+}
+
 function setup() {
   createCanvas(1000, 500)
   background(0)
 
-  const ball = new Ball(50, 500, 250)
+  socket.send(JSON.stringify({
+    payload: {
+      command: "addBall",
+      data: {
+        x: 50,
+        y: 50
+      }
+    }
+  }))
+}
 
-  console.log(ball.r)
+function draw() {
+  if (keyIsDown(LEFT_ARROW)) {
+    move('LEFT')
+  }
 
-  ball.show()
+  if (keyIsDown(RIGHT_ARROW)) {
+    move('RIGHT')
+  }
+
+  if (keyIsDown(UP_ARROW)) {
+    move('UP')
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+    move('DOWN')
+  }
+}
+
+function move(direction) {
+  const message = {
+    payload: {
+      command: 'move',
+      data: {
+        direction
+      }
+    }
+  };
+
+  socket.send(JSON.stringify(message))
 }
